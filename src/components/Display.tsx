a72fdf05f12bd37749acd9d044c879e32d9f107e
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-07-22 23:31:17 +0200

    feat: made colons blink

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,24 +5,32 @@ import moment from 'moment';
 function Display() {
     const [dateAndTime, setDateAndTime] = useState({
         date: '1998-04-13',
-        time: '00:00'
+        time: {
+            hours: '00',
+            minutes: '00'
+        }
     });
+    const [blinkingColon, setBlinkingColon] = useState(false);
 
     const update = () => {
         moment().locale();
         const currentDate = moment().format('YYYY-MM-DD');
-        const currentTime = moment().format('HH:mm');
+        const hours = moment().format('HH');
+        const minutes = moment().format('mm');
 
         const dateAndtime = {
             date: currentDate,
-            time: currentTime
+            time: {
+                hours: hours,
+                minutes: minutes
+            }
         };
-
+        
+        setBlinkingColon((current) => !current);
         setDateAndTime({ ...dateAndtime });
     };
 
     useEffect(() => {
-        update();
         const intervalId = setInterval(() => {
             update();
         }, 1000);
@@ -33,7 +41,7 @@ function Display() {
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2'>
             <div className='text-8xl'>
-                {dateAndTime.time}
+                {dateAndTime.time.hours} <span style={{visibility: blinkingColon ? 'hidden' : 'visible'}}> : </span> {dateAndTime.time.minutes}
             </div>
             <div className='text-3xl flex items-center justify-center p-3'>
                 <div>
